8b3b0017d44dab5cad7ad20fc57931cd81c67dd2
Nick McCoy <33731945+nick-mccoy@users.noreply.github.com>
2020-12-17 15:20:49 -0500

    Add fatal logging (#12)
    * add fatal logging
    
    * add log file
    
    Co-authored-by: Paul Santa Clara <paul@stitchdata.com>

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :url "https://github.com/singer-io/singer-clojure"
   :license {:name "GNU Affero General Public License Version 3; Other commercial licenses available."
             :url "https://www.gnu.org/licenses/agpl-3.0.en.html"}
+  :resource-paths ["resources/base"]
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/data.json "0.2.6"]
 
