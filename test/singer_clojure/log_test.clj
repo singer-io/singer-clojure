a459a5ab0d1ce5f74774f728464066bf9d33a959
Dan Mosora <30501696+dmosorast@users.noreply.github.com>
2021-07-22 19:16:53 +0000

    Adjust tests to match proposed solution

--- test/singer_clojure/log_test.clj
+++ test/singer_clojure/log_test.clj
@@ -11,21 +11,33 @@
        @captured-logs#)))
 
 (deftest log-fatal-trims-error-from-exception []
-  (let [captured-logs (capture-logs (log/log-fatal "Fatal Error Occured" (Exception. "BOOM from Java Exception")))]
-    (is (= 1 (count captured-logs)))
-    (is (= "singer-clojure.log-test - Fatal Error Occured - BOOM from Java Exception" (nth (first captured-logs) 3)))))
+  (let [captured-logs (capture-logs (log/log-fatal "Fatal Error Occured" (Exception. "BOOM from Java Exception")))
+        fatal-logs (filter #(= :error (second %)) captured-logs)]
+    (is (= 2 (count captured-logs)))
+    (is (= 1 (count fatal-logs)))
+    (is (= "singer-clojure.log-test - Fatal Error Occured - BOOM from Java Exception" (nth (first fatal-logs) 3)))
+    (is (= :warn (second (last captured-logs))))))
 
 (deftest log-fatal-trims-error-from-throwable []
-  (let [captured-logs (capture-logs (log/log-fatal "Fatal Error Occured" (proxy [Throwable] ["BOOM from proxy Throwable"])))]
-    (is (= 1 (count captured-logs)))
-    (is (= "singer-clojure.log-test - Fatal Error Occured - BOOM from proxy Throwable" (nth (first captured-logs) 3)))))
+  (let [captured-logs (capture-logs (log/log-fatal "Fatal Error Occured" (proxy [Throwable] ["BOOM from proxy Throwable"])))
+        fatal-logs (filter #(= :error (second %)) captured-logs)]
+    (is (= 2 (count captured-logs)))
+    (is (= 1 (count fatal-logs)))
+    (is (= "singer-clojure.log-test - Fatal Error Occured - BOOM from proxy Throwable" (nth (first fatal-logs) 3)))
+    (is (= :warn (second (last captured-logs))))))
 
 (deftest log-fatal-trims-error-from-exinfo []
-  (let [captured-logs (capture-logs (log/log-fatal "Fatal Error Occured" (ex-info "BOOM" {})))]
-    (is (= 1 (count captured-logs)))
-    (is (= "singer-clojure.log-test - Fatal Error Occured - BOOM" (nth (first captured-logs) 3)))))
+  (let [captured-logs (capture-logs (log/log-fatal "Fatal Error Occured" (ex-info "BOOM" {})))
+        fatal-logs (filter #(= :error (second %)) captured-logs)]
+    (is (= 2 (count captured-logs)))
+    (is (= 1 (count fatal-logs)))
+    (is (= "singer-clojure.log-test - Fatal Error Occured - BOOM" (nth (first fatal-logs) 3)))
+    (is (= :warn (second (last captured-logs))))))
 
 (deftest log-fatal-trims-error-from-wrapped-exinfo []
-  (let [captured-logs (capture-logs (log/log-fatal "Fatal Error Occured"  (ex-info "BOOM" {} (Exception. "Wrapped exception!"))))]
-    (is (= 1 (count captured-logs)))
-    (is (= "singer-clojure.log-test - Fatal Error Occured - BOOM" (nth (first captured-logs) 3)))))
+  (let [captured-logs (capture-logs (log/log-fatal "Fatal Error Occured"  (ex-info "BOOM" {} (Exception. "Wrapped exception!"))))
+        fatal-logs (filter #(= :error (second %)) captured-logs)]
+    (is (= 2 (count captured-logs)))
+    (is (= 1 (count fatal-logs)))
+    (is (= "singer-clojure.log-test - Fatal Error Occured - BOOM" (nth (first fatal-logs) 3)))
+    (is (= :warn (second (last captured-logs))))))
