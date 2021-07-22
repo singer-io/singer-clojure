9196e6219166b9164ec59bed1cb83c2752158121
Collin Simon <csimon@talend.com>
2021-07-22 15:35:08 +0000

    bump to version 1.1.4

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject singer-clojure "1.1.3-SNAPSHOT"
+(defproject singer-clojure "1.1.4"
   :description "Clojure library for shared code between clojure taps"
   :url "https://github.com/singer-io/singer-clojure"
   :license {:name "GNU Affero General Public License Version 3; Other commercial licenses available."
