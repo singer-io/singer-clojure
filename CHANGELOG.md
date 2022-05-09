a7303abff6102589daa627790326bdefeb1e32c9
A. M. Mead <amaiale@gmail.com>
2022-05-09 13:24:40 -0400

    Revert "upgrade log4j version (#17)" (#18)
    * Revert "upgrade log4j version (#17)"
    
    This reverts commit 12bb302051e4a690b09de1ce491af45de152e77e.
    
    * version bump
    
    Co-authored-by: Anne Maiale <amaiale@talend.com>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,11 @@
 # Changelog
 
+## 1.1.7
+  * Revert log4j changes. [#18](https://github.com/singer-io/singer-clojure/pull/18)
+
+## 1.1.6
+  * Use new log4j version and log4j2.properties [#17](https://github.com/singer-io/singer-clojure/pull/17)
+
 ## 1.1.5
   * Prevent fatal logs from showing just the first line of the stack trace (`SEVERE: #error {`, or `FATAL: #error {`, depending on the logging configuration) by moving the full exception to `WARN` level [#16](https://github.com/singer-io/singer-clojure/pull/16)
 
--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject singer-clojure "1.1.6"
+(defproject singer-clojure "1.1.7"
   :description "Clojure library for shared code between clojure taps"
   :url "https://github.com/singer-io/singer-clojure"
   :license {:name "GNU Affero General Public License Version 3; Other commercial licenses available."
@@ -12,10 +12,7 @@
                  [cider/cider-nrepl "0.17.0"]
 
                  ;; Basic log4j dependency to declare bare minimum
-                 [org.clojure/tools.logging "1.2.4"]
-                 [org.slf4j/slf4j-log4j12 "1.7.36"]
-                 [org.apache.logging.log4j/log4j-1.2-api "2.17.1"]
-                 [org.apache.logging.log4j/log4j-core "2.17.1"]
+                 [log4j "1.2.17"]
 
                  [org.clojure/tools.cli "0.4.1"]]
   :target-path "target/%s"
